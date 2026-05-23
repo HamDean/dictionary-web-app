af970aea49bf65858c1efe3a17a7ced479f79ff2
Hamdeen <hamdeen247@gmail.com>
2026-05-23 17:46:14 +0000

    Set up navbar component and add logo

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import logo from "../assets/Logo.svg";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div>
+        <img className="logo" src={logo} />{" "}
+      </div>
+      <div></div>
+      <div></div>
+    </nav>
+  );
+};
+export default NavBar;
