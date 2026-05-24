b093421a7450ad65b86478b720393c05b0aedd20
Hamdeen <hamdeen247@gmail.com>
2026-05-24 22:45:40 +0000

    Refactor: Update Source component to use anchor tag for source links

--- src/components/Source.tsx
+++ src/components/Source.tsx
@@ -5,7 +5,10 @@ const Source = ({ source }: { source: string }) => {
     <div className="flex flex-col md:flex-row gap-0.5 md:gap-5 text-[14px] md:text-xl">
       <p className="text-[#757575] underline">Source</p>
       <span className="text-[#2D2D2D] underline flex items-center gap-2.5">
-        <span>{source}</span> <img className="h-3 w-3" src={linkIcon} alt="Link" />
+        <a href={source} target="_blank" rel="noopener noreferrer">
+          {source}
+        </a>
+        <img className="h-3 w-3" src={linkIcon} alt="Link" />
       </span>
     </div>
   );
