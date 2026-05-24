5a9c617a31ccb71bcc577291fda9fcd25aa52280
Hamdeen <hamdeen247@gmail.com>
2026-05-24 20:19:01 +0000

    Refactor App component to conditionally render WordHeader, Source, and separator based on loading state

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,25 @@ import useDefinitions from "./hooks/useDefinitions";
 
 const App = () => {
   const { definition, setWord } = useDefinitions();
-
-  // TODO: handle initial load and error states
+  const isLoaded = definition[0]?.meanings?.length > 0;
 
   return (
     <div className="container">
       <NavBar />
       <main className="mt-6 md:mt-14">
         <SearchBar onSearch={(word) => setWord(word)} />
-        <WordHeader
-          word={definition[0]?.word}
-          phonetic={definition[0]?.phonetic}
-          audioSources={definition[0]?.phonetics}
-        />
+        {isLoaded && (
+          <WordHeader
+            word={definition[0]?.word}
+            phonetic={definition[0]?.phonetic}
+            audioSources={definition[0]?.phonetics}
+          />
+        )}
         <Definitions meanings={definition[0]?.meanings || []} />
-        <hr className="text-[#E9E9E9] mt-12 mb-5" />
+        {isLoaded && <hr className="text-[#E9E9E9] mt-12 mb-5" />}
       </main>
       <footer>
-        <Source source={definition[0]?.sourceUrls[0]} />
+        {isLoaded && <Source source={definition[0]?.sourceUrls[0]} />}
       </footer>
     </div>
   );
